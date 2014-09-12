daaa777bc38fc5211d1d281d8edbf77960a2159a
Gaurav Gupta <tech_gaurav@hotmail.com>
2014-09-12 07:08:03 +0100

    @

--- App_Start/WebApiConfig.cs
+++ App_Start/WebApiConfig.cs
@@ -6,7 +6,7 @@ namespace TryMVC
     {
         public static void Register(HttpConfiguration config)
         {
-            config.MapHttpAttributeRoutes();
+            //config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
